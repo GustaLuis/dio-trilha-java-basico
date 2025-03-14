bb80db586006f88cb36ca22e75ad6fa61b34a22b
GustaLuis <luismouraalencar@gmail.com>
2025-03-14 20:47:20 -0300

    projeto basico java banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,8 +1,26 @@
-import java.util.Scanner
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o Numero da Conta: ");
+        int numConta = scanner.nextInt();
+        scanner.nextLine();
         
+        System.out.println("Digite sua Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o Nome do Titular da Conta: ");
+        String titular = scanner.nextLine();
+
+        System.out.println("Digite o Valor do seu Deposito Inicial:" );
+        double valorDepos = scanner.nextDouble();
+
+        scanner.close();
+
+        //exibir
+        System.out.println("Olá " + titular + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numConta + " e seu saldo " + valorDepos + " já está disponível para saque.");
+
     }
 }
